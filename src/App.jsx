f0c66ec32dd2ac5464901f8a98f4df935b125916
vyen <valmyen@icloud.com>
2024-11-07 17:09:44 -0500

    Removed images to test deployment

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>no image womp womp</p>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
