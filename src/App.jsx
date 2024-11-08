b7bd261184bdea770d75ebf5bb354585a0609f96
vyen <valmyen@icloud.com>
2024-11-07 19:18:12 -0500

    Replace images

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -7,7 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <p>no image womp womp</p>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
